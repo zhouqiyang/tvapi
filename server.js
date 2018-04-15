4c069f1358df3c0b5ee3f1d333c62b6298a423bf
zhouqiyang <37770399+zhouqiyang@users.noreply.github.com>
2018-04-15 11:34:51 +0800

    Update server.js

--- server.js
+++ server.js
@@ -77,6 +77,27 @@ app.get('/', function (req, res) {
   }
 });
 
+app.get('/canteen', function (req, res) {
+  // try to initialize the db on every request if it's not already
+  // initialized.
+  if (!db) {
+    initDb(function(err){});
+  }
+  if (db) {
+    var col = db.collection('counts');
+    // Create a document with request IP and current time of request
+    col.insert({ip: req.ip, date: Date.now()});
+    col.count(function(err, count){
+      if (err) {
+        console.log('Error running count. Message:\n'+err);
+      }
+      res.render('canteen.html', { pageCountMessage : count, dbInfo: dbDetails });
+    });
+  } else {
+    res.render('canteen.html', { pageCountMessage : null});
+  }
+});
+
 app.get('/pagecount', function (req, res) {
   // try to initialize the db on every request if it's not already
   // initialized.
