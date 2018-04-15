24bedfdca7da9243dc63cbf94aa45b5bed95f45b
zhouqiyang <37770399+zhouqiyang@users.noreply.github.com>
2018-04-15 13:39:41 +0800

    Update server.js

--- server.js
+++ server.js
@@ -101,7 +101,7 @@ app.get('/canteen', function (req, res) {
 
 app.get('/canteen/img/:img?', function (req, res, next) {
   if (req.params.img){
-    var img = fs.readFileSync(process.cwd() + '/Views/img/' + req.params.img, 'binary');
+    var img = fs.readFileSync(process.cwd() + '/views/img/' + req.params.img, 'binary');
     res.writeHead('200', {'Content-Type': 'image/jpeg'});
     res.end(img, 'binary');
   } else {
